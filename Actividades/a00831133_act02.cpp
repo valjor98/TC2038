36552ca5500bf390cb92a98820b8c26e4fb634c0
valjor98 <76230623+valjor98@users.noreply.github.com>
2022-08-15 11:41:53 -0500

    ya

--- Actividades/a00831133_act02.cpp
+++ Actividades/a00831133_act02.cpp
@@ -11,6 +11,18 @@
 #include <cmath>
 using namespace std;
 
+void tokenize(string const &str, const char delim,
+            vector<string> &out)
+{
+    // construct a stream from the string
+    stringstream ss(str);
+ 
+    string s;
+    while (getline(ss, s, delim)) {
+        out.push_back(s);
+    }
+}
+
 int main(){
 
     int m;
@@ -20,7 +32,7 @@ int main(){
     getline(cin, oracion);
     m = stoi(n);
     vector<char> vectorCodigo;
-    vector<string> vectorCasos1, vectorCasos2, vectorCasos3;
+    vector<string> vectorCasos1, vectorCasos2, vectorCasos3, resultt;
     vector<int> vectorCasos4;
     int counter = 0;
     for(int i = 0; i < 14; i++){
@@ -35,18 +47,27 @@ int main(){
         {vectorCodigo[12], '*'}, {vectorCodigo[13], '/'}, {' ', ' '}
         };
 
+/*     for(int i = 0; i < vectorCodigo.size(); i++){
+        cout << vectorCodigo[i] << endl;
+    } */
+        
 
     for(int i = 0; i < m; i++){
         getline(cin, caso);
+        cout << caso << endl;
         for(int j = 0; j < caso.size(); j++){
-            char x = caso[j];
-            char y = umap[x];
-            replace( caso.begin(), caso.end(), x, y);  
+            char xerr = caso[j];
+            char yerr = umap[caso[j]];
+            replace( caso.begin(), caso.end(), xerr, yerr);  
         }
         string parte1, parte3;
         string parte2;
-        stringstream s(caso);
-        s>>parte1>>parte2>>parte3;
+        stringstream ss(caso);
+        
+        ss>>parte1>>parte2>>parte3;
+        tokenize(caso, ' ', resultt);
+        cout << resultt[2] << endl;;
+        //cout << "UWw  " << parte1 << " " << parte2 << " " << parte3  << endl;
         
         int parte1INT = stoi(parte1);
         int parte3INT = stoi(parte3);
