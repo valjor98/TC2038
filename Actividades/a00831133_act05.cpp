8e3f12c8b7b4bcc3ed7e03308eb3947bb2fba5dc
valjor98 <76230623+valjor98@users.noreply.github.com>
2022-09-04 19:28:08 -0500

    changes to act

--- Actividades/a00831133_act05.cpp
+++ Actividades/a00831133_act05.cpp
@@ -5,26 +5,26 @@
 
 using namespace std;
 
-const int maxn = 1002;
-int aa[maxn], bb[maxn], cc[maxn << 1];
-int mat[maxn][maxn];
+const int maxNum = 1002;
+int aa[maxNum], bb[maxNum], cc[maxNum*2];
+int mat[maxNum][maxNum];
 int n1, n2;
-bool flag;
+bool deteccion;
 vector<string> resultados;
 
 void dfs(int lena, int lenb, int lenc){
     if(lena == n1+1 && lenb == n2+1){
-            flag=1;
+            deteccion=1;
         return;
     }
     if(mat[lena][lenb]){
         return; 
     }
-    if(!flag && aa[lena] == cc[lenc]){
+    if(!deteccion && aa[lena] == cc[lenc]){
         mat[lena][lenb] = 1;
         dfs(lena+1, lenb, lenc+1);
     }
-    if(!flag && bb[lenb] == cc[lenc]){
+    if(!deteccion && bb[lenb] == cc[lenc]){
         mat[lena][lenb] = 1;
         dfs(lena,lenb+1, lenc+1);
     }
@@ -44,10 +44,10 @@ int main(){
         input(n2, bb);
         bb[n2+1] = -1;
         input(n1+n2, cc);
-        flag=0;
+        deteccion=0;
         memset(mat, 0, sizeof(mat));
         dfs(1,1,1);
-        if(flag){
+        if(deteccion){
             resultados.push_back("possible");
         }
         else {
