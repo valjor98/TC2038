1f4c2811f277a77739d797757b0006e85d6e4c3e
valjor98 <76230623+valjor98@users.noreply.github.com>
2022-09-11 18:17:49 -0500

    changes

--- Actividades/a00831133_act06.cpp
+++ Actividades/a00831133_act06.cpp
@@ -3,62 +3,58 @@
 #include <string.h>
 #include <vector>
 
-
 using namespace std;
 
 vector<string> resultados;
 
 int Substring (char *cUno, char *cDos, int m, int n){
-    int iMatA [m+1][n+1];
-    int iMax = 0;
+    int MatA [m+1][n+1];
+    int Max = 0;
     
     for (int iR = 0; iR <= m; iR++){
         for (int iA = 0; iA <= n; iA++){
             if (iR == 0 || iA == 0){
-                iMatA[iR][iA] = 0;
+                MatA[iR][iA] = 0;
             }
             else if (cUno[iR-1] == cDos[iA-1]){
-                
-                iMatA[iR][iA] = iMatA[iR-1][iA-1] + 1;
-                
-                iMax = (iMax > iMatA[iR][iA]) ? iMax : iMatA[iR][iA];
+                MatA[iR][iA] = MatA[iR-1][iA-1] + 1;
+                Max = (Max > MatA[iR][iA]) ? Max : MatA[iR][iA];
             }
             else{
-                 iMatA[iR][iA] = 0;
+                 MatA[iR][iA] = 0;
             }
         }
     }
-    return iMax;
+    return Max;
 }
 
 int main(){
-    int iCasos = 0;
-    int slineatam = 0, slinea2tam = 0;
+    int numCasos = 0;
+    int lineatam = 0, linea2tam = 0;
     int iN = 0;
-    string sLinea = "";
-    string sLinea2 = "";
+    string Linea = "";
+    string Linea2 = "";
     char cUno [1000];
     char cDos [1000];
     
-    cin >> iCasos;
+    cin >> numCasos;
     
-    while (iCasos > iN){
-        cin >> sLinea;
-        slineatam = sLinea.length();
+    while (numCasos > iN){
+        cin >> Linea;
+        lineatam = Linea.length();
         
-        for (int iR = 0; iR < slineatam; iR++){
-            cUno[iR] = sLinea[iR];
+        for (int iR = 0; iR < lineatam; iR++){
+            cUno[iR] = Linea[iR];
         }
         
-        cin>> sLinea2;
-        slinea2tam = sLinea2.length();
+        cin>> Linea2;
+        linea2tam = Linea2.length();
         
-        for (int iA = 0; iA < slinea2tam; iA++){
-            cDos[iA] = sLinea2[iA];
+        for (int iA = 0; iA < linea2tam; iA++){
+            cDos[iA] = Linea2[iA];
         }
-        
 
-        string resul = "Case " + to_string(iN+1) + ": " +to_string(Substring(cUno, cDos, slineatam, slinea2tam)) ;
+        string resul = "Case " + to_string(iN+1) + ": " +to_string(Substring(cUno, cDos, lineatam, linea2tam)) ;
         resultados.push_back(resul);
         iN++;
     }
@@ -66,7 +62,5 @@ int main(){
     for(int i = 0; i < resultados.size(); i++){
         cout << resultados[i]<< endl;
     }
-
-    
     return 0;
 }
--- Actividades/a00831133_act07.cpp
+++ Actividades/a00831133_act07.cpp
@@ -7,69 +7,58 @@ using namespace std;
 
 vector<string> resultados;
 
-int subsequence(char *cUno, char *cDos, int m, int n)
-{
-    int iMatA [m+1][n+1];
+int subsequence(char *cUno, char *cDos, int m, int n){
+    int MatA [m+1][n+1];
     
-    for (int iR = 0; iR <= m; iR++)
-    {
-        for (int iA = 0; iA <= n; iA++)
-        {
-            if (iR == 0 || iA == 0)
-            {
-                iMatA[iR][iA] = 0;
+    for (int iR = 0; iR <= m; iR++){
+        for (int iA = 0; iA <= n; iA++){
+            if (iR == 0 || iA == 0){
+                MatA[iR][iA] = 0;
             }
             else if (cUno[iR-1] == cDos[iA-1]){
-                
-                iMatA[iR][iA] = iMatA[iR-1][iA-1] + 1;
+                MatA[iR][iA] = MatA[iR-1][iA-1] + 1;
             }
-            else
-                iMatA[iR][iA] = ((iMatA[iR-1][iA] > iMatA[iR][iA-1]) ? iMatA[iR-1][iA]: iMatA[iR][iA-1]);
+            else{
+                MatA[iR][iA] = ((MatA[iR-1][iA] > MatA[iR][iA-1]) ? MatA[iR-1][iA]: MatA[iR][iA-1]);
+            }
         }
     }
-    return iMatA[m][n];
-
+    return MatA[m][n];
 }
-int main()
-{
-    int iCasos = 0;
-    
-    int slineatam = 0, slinea2tam =0;
+
+int main(){
+    int numCasos = 0;
+    int lineatam = 0, linea2tam =0;
     int iN = 0;
-    string sLinea = " ";
-    string sLinea2 = " ";
+    string Linea = " ";
+    string Linea2 = " ";
     char cUno [1000];
     char cDos [1000];
     
-    cin >> iCasos;
+    cin >> numCasos;
     
-    while (iCasos > iN)
-    {
-        cin>> sLinea;
-        slineatam = sLinea.length();
+    while (numCasos > iN){
+        cin>> Linea;
+        lineatam = Linea.length();
         
-        for (int iR = 0; iR < slineatam; iR++)
-        {
-            cUno[iR] = sLinea[iR];
+        for (int iR = 0; iR < lineatam; iR++){
+            cUno[iR] = Linea[iR];
         }
         
-        cin>>sLinea2;
-        slinea2tam = sLinea2.length();
+        cin>>Linea2;
+        linea2tam = Linea2.length();
         
-        for (int iA = 0; iA < slinea2tam; iA++)
-        {
-            cDos[iA] = sLinea2[iA];
+        for (int iA = 0; iA < linea2tam; iA++){
+            cDos[iA] = Linea2[iA];
         }
-        string resul = "Case " + to_string(iN+1) + ": " +to_string(subsequence(cUno, cDos, slineatam, slinea2tam)) ;
+        string resul = "Case " + to_string(iN+1) + ": " +to_string(subsequence(cUno, cDos, lineatam, linea2tam)) ;
         resultados.push_back(resul);
-        //cout<<"Case "<<iN + 1<<": "<<subsequence(cUno, cDos, slineatam, slinea2tam)<<endl;
         iN++;
-        slineatam=0;
-        slinea2tam=0;
+        lineatam=0;
+        linea2tam=0;
     }
     for(int i = 0; i < resultados.size(); i++){
         cout << resultados[i]<< endl;
     }
-   
     return 0;
 }
