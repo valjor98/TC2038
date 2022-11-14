58d23095503c2d2dd4722f8da96f895dcf1c8c7b
valjor98 <valjor98@gmail.com>
2022-11-14 12:24:05 -0600

    changes

--- Actividades en clase/closest.cpp
+++ Actividades en clase/closest.cpp
@@ -34,30 +34,62 @@ float dist(Point &p1, Point &p2){
 }
 
 float bruteForce(vector<Point> &P, int ini, int fin){
+    cout << "brute force" << endl;
     float minAux = FLT_MAX;
     for(int i = ini; i < fin; i++){
+        cout << "brute force AAAAAAAAAAAAAAAAAA" << endl;
         for(int j = i+1; i < fin; j++){
             minAux = min(minAux, dist(P[i], P[j]));
+            cout << "brute force BBBBB" << endl;
         }
     }
     return minAux;
 }
 
+float stripClosest(vector<Point> &strip, float dMenor){
+    cout << "strip" << endl;
+    float minStrip = dMenor;
+    sort(strip.begin(), strip.end(), compareY);
+    for(int i = 0; i < strip.size()-1 ; i++){
+        for(int j = i+1; j < strip.size() && (strip[j].y - strip[i].y) < minStrip; j++  ){
+            minStrip = min(minStrip, dist(strip[i], strip[j]));
+        }
+    }
+    cout << "strip" << endl;
+    return minStrip;
+}
+
 float closest_Helper(vector<Point> &P, int ini, int fin){
     // si en rango solo hay 3 o menos hacerlo por fuerza bruta
     if( fin-ini < 3){
+        cout << "sexo 7" << endl;
         return bruteForce(P, ini, fin);
     }
+    cout << "sexo 3" << endl;
     int mid = (ini+fin)/2;
     Point midPoint = P[mid];
     float dl = closest_Helper(P, ini, mid);
     float dr = closest_Helper(P, mid+1, fin);
     float dMenor = min(dl, dr);
+    cout << "sexo 4" << endl;
+    vector <Point> strip;
+    for(int i = ini ; i < fin ; i++){
+        if(abs(P[i].x - midPoint.x) < dMenor){
+            strip.push_back(P[i]);
+        }
+    }
+    cout << "sexo 5" << endl;
+    float ds = stripClosest(strip, dMenor);
+    dMenor = min(dMenor, ds);
+    cout << "sexo 6" << endl;
+    return dMenor;
 }
-
+// compledidad O(nlog(n) 
 float closest(vector<Point> &P){
     //Ordenr con respeccto a X
+    cout << "sexo 1" << endl;
     sort(P.begin(), P.end(), compareX);
+    cout << "sexo 3" << endl;
     return closest_Helper(P, 0, P.size() - 1);
 }
 
